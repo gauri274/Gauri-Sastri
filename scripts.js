436f57936e03aa20897a51cf0df1cefe903d7eef
gauri274 <srigauri.sastri@flame.edu.in>
2024-11-12 16:32:09 +0530

    Add files via upload
    fist draft of portfolio website

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.header-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop - 60,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
